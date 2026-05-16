8396b318b8cdbe20112c3581c32042f499b39992
E'zoza <ezozanormuxamatova@gmail.com>
2026-05-16 22:03:34 +0500

    Add files via upload

--- main.py
+++ main.py
@@ -3,7 +3,8 @@ import os
 import asyncio
 import edge_tts
 from telebot import types
-bot = telebot.TeleBot(token=os.environ.get("BOT_TOKEN"))
+
+bot = telebot.TeleBot(token="8958790619:AAGRloCEKRk7oRplD4oBxeo5klz-gIaiNTE")
 
 # Store user state: {user_id: {"text": ..., "lang": ...}}
 user_state = {}
